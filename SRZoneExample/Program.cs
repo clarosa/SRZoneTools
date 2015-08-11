5f7574d22b82fc6690f7842952b1cd4e3dc7d707
Chris <clarosa@clarosa.info>
2015-08-10 20:38:20 -0400

    Added example program which uses the zone file library DLL.

---
+++ SRZoneExample/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using ChrisLaRosa.SaintsRow.ZoneFile;
+
+namespace SRZoneExample
+{
+    /// <summary>
+    /// This very simple example program reads all sections in a ".czn_pc" file,
+    /// parses the objects section data, and prints the value of all string properties.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SRZoneCombinedFile file = new SRZoneCombinedFile(args[0]);
+            foreach (SRZoneSection section in file.SectionList)
+                if (section.CpuData is SRZoneObjectSectionCpuData)
+                    foreach (SRZoneObject zoneObject in ((SRZoneObjectSectionCpuData)section.CpuData).ObjectList)
+                        foreach (SRZoneProperty zoneProperty in zoneObject.PropertyList)
+                            if (zoneProperty is SRZoneStringProperty)
+                                Console.WriteLine(zoneProperty.ToString());
+        }
+    }
+}
